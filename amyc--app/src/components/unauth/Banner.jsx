c16ab8c99f3c07b4a238fcc01f1ba62791c1247a
Bushra Aksoy <bbxsra17@gmail.com>
2023-08-04 09:25:50 +0300

    made some style changes

--- amyc--app/src/components/unauth/Banner.jsx
+++ amyc--app/src/components/unauth/Banner.jsx
@@ -1,19 +1,16 @@
-import banner from '../../images/quran1.jpg'
-import '../../styles/Banner.css'
-
+import banner from "../../images/quran1.jpg";
+import "../../styles/Banner.css";
 
 const Banner = () => {
-    const goToDashboard = () => {
-        window.location.pathname = '/dashboard'
-    }
+  const goToDashboard = () => {
+    window.location.pathname = "/dashboard";
+  };
 
-    return (
-        <div className='banner'>
-            <img className='banner-img' src={banner} alt='banner'/>
-            <h1>Welcome to Ansaar Muslim Youth Center Project</h1>
-            <button onClick={goToDashboard} className='signup-btn'>Sign up</button>
-        </div>
-    )
-}
+  return (
+    <div className="banner">
+      <h1>Welcome to Ansaar Muslim Youth Center Project</h1>
+    </div>
+  );
+};
 
 export default Banner;
